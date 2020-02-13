cfe537a57525511c06ae3111417fc3b392a0ca3c
xjk-lenovo <1532398723@qq.com>
2020-02-13 12:25:39 +0800

    --stable edit sysclear

--- common/sysclear/controls.py
+++ common/sysclear/controls.py
@@ -5,5 +5,6 @@ def clearEmpDir(basePath):
     for root,dir,files in os.walk(basePath,topdown=False):
         if not files:
             os.rmdir(root)
+    return '{}清理空文件完成'.format(basePath)
 
 # clearEmpDir(r'D:\projects\zfjw\common\expfiles\mooc')
--- dailyMain.py
+++ dailyMain.py
@@ -4,6 +4,7 @@ from databaseconfig.connectdbs import connect
 from common.fileAction.controls import fileInfo
 import time
 from apps.mails import mailInterface as mail
+from common.sysclear import controls as sysClear
 
 def mailmain(con):
     '''回复学位中心中心的日常任务！'''
@@ -56,6 +57,7 @@ def main():
     # L=L+wlzxmain(con)+jwxtmain(con)+mailmain(con)
     logsname='D:\\projects\\zfjw\\logs\\'
     txt=fileInfo(logsname+time.strftime('%Y-%m-%d',time.localtime())+'.txt')
+    L.append(sysClear.clearEmpDir(r'D:\projects\zfjw\common\expfiles\mooc'))
     end=time.perf_counter()
     L.append('共耗时{}秒'.format(str(end-start)))
     L.append('*'*30)
