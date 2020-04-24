f9f73624337b66d5e8acfbbeb23726b3f9f4e6fe
80662 <1532398723@qq.com>
2020-04-24 23:08:23 +0800

    --add xfyqjd

--- apps/jwglxt/jxzxjh/fzmkjd/controls.py
+++ apps/jwglxt/jxzxjh/fzmkjd/controls.py
@@ -4,22 +4,26 @@ import uuid
 # insert into JW_JH_JXZXJHXFYQXXB
 # (JXZXJHXX_ID,XFYQJD_ID,XFYQJDMC,JDKCSX,ZYFX_ID,YQZDXF,FXFYQJD_ID,px,SFMJD,KCXZDM,XDLX,JCBJ,SFZDJS,xfyqzjdgx)
 con=connect()
-jxzxjhxx_id_s=con.execute(vars.getJxzxjhZyxx)
+njdm_id='2016'
+jxzxjhxx_id_s=con.execute(vars.getJxzxjhZyxx.format(njdm_id))
+print(jxzxjhxx_id_s)
 for jxzxjhxx_id in jxzxjhxx_id_s:
-    fxfyqjd_id_s=con.execute(vars.getXfyqjdXx.format(jxzxjhxx_id))###
+    print(vars.getXfyqjdXx.format(jxzxjhxx_id[0]))
+    fxfyqjd_id_s=con.execute(vars.getXfyqjdXx.format(jxzxjhxx_id[0]))###
     ###(xfyqjd_id,xfyqjdmc,yqzdxf,px)
-    jdlist=[]
     for fxfyqjd_id in fxfyqjd_id_s:
+        jdlist = []
         xfyqjd_id_A=str(uuid.uuid1()).replace('-','')
         xfyqjd_id_B=str(uuid.uuid1()).replace('-','')
+        print('子模块A',xfyqjd_id_A,'新模块B',xfyqjd_id_B)
         xfyqjd_mc_a = '子模块A'
         xfyqjd_mc_b = fxfyqjd_id[1] + 'B'
         kcxz='16' if fxfyqjd_id[1].find('实践环节')>=0 else None
         zmk_a=(
-            jxzxjhxx_id,xfyqjd_id_A,xfyqjd_mc_b,'1','wfx','0',fxfyqjd_id[0],int(fxfyqjd_id[3])+1,'1',kcxz,'zx','1','0','1'
+            jxzxjhxx_id[0],xfyqjd_id_A,xfyqjd_mc_a,'1','wfx','0',fxfyqjd_id[0],int(fxfyqjd_id[3])+1,'1',kcxz,'zx','1','0','1'
         )
         zmk_b=(
-            jxzxjhxx_id, xfyqjd_id_A, xfyqjd_mc_b, '1', 'wfx', fxfyqjd_id[2], fxfyqjd_id[0], int(fxfyqjd_id[3]) + 2, '1', kcxz,
+            jxzxjhxx_id[0], xfyqjd_id_B, xfyqjd_mc_b, '1', 'wfx', fxfyqjd_id[2], fxfyqjd_id[0], int(fxfyqjd_id[3]) + 2, '1', kcxz,
             'zx', '1', '0', '1'
         )
         jdlist.append(zmk_a)
@@ -31,4 +35,4 @@ for jxzxjhxx_id in jxzxjhxx_id_s:
             con.execute(vars.upJhkc.format(xfyqjd_id_B,fxfyqjd_id[0]))
             ###更新父节点关系
             con.execute(vars.upFjdxx.format(fxfyqjd_id[0]))
-            print()
+            print('执行完成！')
